7203478b12c4bdcdf39ba97c67693a26b50144fe
liu-du <duliujimmy@hotmail.com>
2018-11-28 08:43:52 +1100

    use pug as templating engine

--- nodejs_project/routes/admin.js
+++ nodejs_project/routes/admin.js
@@ -4,16 +4,19 @@ const express = require('express');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     console.log('In add-product!');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' })
 });
 
 // /admin/product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
--- nodejs_project/routes/shop.js
+++ nodejs_project/routes/shop.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const rootDir = require('../util/path');
 const express = require('express');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products
+    console.log('shop.js', adminData.products);
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
