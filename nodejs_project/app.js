0d66bb0392bfb4bb1d03d4bdf8f018da185be558
liu-du <duliujimmy@hotmail.com>
2018-11-29 07:56:35 +1100

    clean up

--- nodejs_project/app.js
+++ nodejs_project/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
