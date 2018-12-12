cc94817043f5ab5e63350a0e8c97d7d21415523c
liu-du <duliujimmy@hotmail.com>
2018-12-12 22:21:42 +1100

    Use Mongoose ODM (Object Doucument Mapper), add add product page

--- nodejs_project/app.js
+++ nodejs_project/app.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const app = express();
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 app.set('view engine', 'ejs');
@@ -15,20 +15,25 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById("5c0f8ac38a7dd0056dced5c5")
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
-});
+
+// app.use((req, res, next) => {
+//     User.findById("5c0f8ac38a7dd0056dced5c5")
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
+// use mongoose ORM (Object Document Mapper)
+mongoose
+    .connect('mongodb+srv://jimmy:mac.2018@cluster0-mzvi0.mongodb.net/shop?retryWrites=true')
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(console.log);
