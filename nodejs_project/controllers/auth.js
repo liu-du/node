5e7592866329f0e16ce5718a25794ac232898bad
liu-du <duliujimmy@hotmail.com>
2019-01-14 08:38:56 +1100

    Validate Email with express-validator and add custom validator

--- nodejs_project/controllers/auth.js
+++ nodejs_project/controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const sendGridApi = require('../misc/sendGridApi');
+const { validationResult } = require('express-validator/check');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -84,6 +85,17 @@ exports.postSignup = (req, res, next) => {
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).
+            render('auth/signup.ejs', {
+                path: '/signup',
+                pageTitle: 'Sign up',
+                errorMessage: errors.array()[0].msg,
+                messageType: 'error'
+            });
+    }
 
     User.findOne({email: email})
         .then(userDoc => {
--- nodejs_project/routes/auth.js
+++ nodejs_project/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
+const { check } = require('express-validator/check');
 
 router.get('/login', authController.getLogin);
 
@@ -10,7 +11,17 @@ router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {req}) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden.');
+            }
+            return true;
+        }),
+    authController.postSignup);
 
 router.get('/reset', authController.getReset);
 
