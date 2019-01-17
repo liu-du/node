f1890fb44e51ebcaadce394e1480372267fcec7c
liu-du <duliujimmy@hotmail.com>
2019-01-17 21:29:34 +1100

    Add pagination

--- nodejs_project/controllers/shop.js
+++ nodejs_project/controllers/shop.js
@@ -4,15 +4,32 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-    // fectchAll takes a call back so it doesn't block!
-    Product.find()
-        .then(products => {
-            res.render('shop/product-list', {
-                prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
-            });
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find().countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product
+                .find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+                .then(products => {
+                    res.render('shop/product-list', {
+                        prods: products,
+                        pageTitle: 'All Products',
+                        path: '/products',
+                        currentPage: page,
+                        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                        hasPreviousPage: page > 1,
+                        nextPage: page + 1,
+                        previousPage: page - 1,
+                        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+                    });
+                })
         })
         .catch(err => {
             console.log(err);
@@ -43,14 +60,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product
-        .find()
-        .then(products => {
-            res.render('shop/index', {
-                prods: products,
-                pageTitle: 'Shop',
-                path: '/'
-            });
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find().countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product
+                .find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+                .then(products => {
+                    res.render('shop/index', {
+                        prods: products,
+                        pageTitle: 'Shop',
+                        path: '/',
+                        currentPage: page,
+                        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                        hasPreviousPage: page > 1,
+                        nextPage: page + 1,
+                        previousPage: page - 1,
+                        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+                    });
+                })
         })
         .catch(err => {
             console.log(err);
@@ -183,7 +215,7 @@ exports.getInvoices = (req, res, next) => {
             if (order.user.userId.toString() !== req.user._id.toString()) {
                 return next(new Error('Unauthorized'));
             }
-            
+
             const pdfDoc = new PDFDocument();
             pdfDoc.pipe(fs.createWriteStream(invoicePath));
 
