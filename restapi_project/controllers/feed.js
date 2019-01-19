fe2b8d1db587a0a177afd5d9e590e7a4a6fc3e0b
liu-du <duliujimmy@hotmail.com>
2019-01-19 23:06:58 +1100

    Connect backend and frontend

--- restapi_project/controllers/feed.js
+++ restapi_project/controllers/feed.js
@@ -1,15 +1,35 @@
 exports.getPosts = (req, res, next) => {
     return res.status(200).json({
-        posts: [{title: 'First post', content: 'This is the first post!'}]
+        posts: [
+            { 
+                _id: '1',
+                title: 'First post', 
+                creator: {
+                    name: 'Jimmy'
+                },
+                content: 'This is the first post!', 
+                imageUrl: 'images/1005455253.jpg',
+                createdAt: new Date()
+            }
+        ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    console.log(req.body);
     const title = req.body.title;
     const content = req.body.content;
     // create post in db
     res.status(201).json({
         message: 'Post created successfully!',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            _id: '3',
+            title: title, 
+            content: content,
+            creator: {
+                name: 'Jimmy'
+            },
+            createdAt: new Date()
+        }
     })
 };
