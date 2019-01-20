3b078c58699774bc318148bb32a19774feac118b
liu-du <duliujimmy@hotmail.com>
2019-01-20 11:37:56 +1100

    Add Static Image serving and Error Handling

--- restapi_project/app.js
+++ restapi_project/app.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -8,6 +9,8 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded, used for data submitted through form
 app.use(bodyParser.json()); // application/json
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   // Allow request from all IPs, Otherwise CORS (Cross Origin Resource Sharing) errors
@@ -24,16 +27,23 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const statusCode = error.statusCode || 500;
+  const message = error.message;
+  res.status(statusCode).json({ message: message });
+});
+
 mongoose
   .connect(
-    "mongodb+srv://jimmy:mac.2018@cluster0-mzvi0.mongodb.net/messages?retryWrites=true"
+    'mongodb+srv://jimmy:mac.2018@cluster0-mzvi0.mongodb.net/messages?retryWrites=true'
   )
   .then(result => {
     app.listen(8080);
   })
   .then(result => {
-    console.log("\nConnected\n");
+    console.log('\nConnected\n');
   })
   .catch(err => {
-    console.log("Error: ", err);
+    console.log('Error: ', err);
   });
--- restapi_project/controllers/feed.js
+++ restapi_project/controllers/feed.js
@@ -1,43 +1,39 @@
 const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
+const errHandler = require('../misc/error-handler');
 
 exports.getPosts = (req, res, next) => {
-  return res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First post',
-        creator: {
-          name: 'Jimmy'
-        },
-        content: 'This is the first post!',
-        imageUrl: 'images/1005455253.jpg',
-        createdAt: new Date()
-      }
-    ]
-  });
+  Post.find()
+    .then(posts => {
+      return res.status(200).json({
+        message: 'Fetch posts success',
+        posts: posts
+      });
+    })
+    .catch(err => next(errHandler(err)));
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation Failed...',
-      errors: errors.array()
-    });
+    const error = new Error('Validation Failed...');
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
+  console.log(req.body);
   // create post in db
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/761541913.jpg',
+    imageUrl: 'images/210091320.jpg',
     creator: {
       name: 'Jimmy'
     }
   });
+
   post
     .save()
     .then(result => {
@@ -48,7 +44,23 @@ exports.createPost = (req, res, next) => {
         createdAt: new Date()
       });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => next(errHandler(err)));
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode(404);
+        throw error;
+      }
+
+      return res.status(200).json({
+        message: 'Post fetched',
+        post: post
+      });
+    })
+    .catch(err => next(errHandler(err)));
 };
