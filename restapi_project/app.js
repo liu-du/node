43342c60d2fe7e9a42db6a89cc8460a1bc71f75f
liu-du <duliujimmy@hotmail.com>
2019-01-20 13:34:09 +1100

    Add file uploads/downloads with multer

--- restapi_project/app.js
+++ restapi_project/app.js
@@ -2,14 +2,39 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, new Date().toISOString() + '.' + file.mimetype.split('/')[1]);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded, used for data submitted through form
 app.use(bodyParser.json()); // application/json
-
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
--- restapi_project/controllers/feed.js
+++ restapi_project/controllers/feed.js
@@ -21,14 +21,21 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
+
+  if (!req.file) {
+    const error = new Error('No image provided...');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-  console.log(req.body);
   // create post in db
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/210091320.jpg',
+    imageUrl: imageUrl,
     creator: {
       name: 'Jimmy'
     }
